6acc5b5a48ce68b2e45a3c02b51ec984d467a450
Martin Hanzel <arthanzel@gmail.com>
2018-03-08 14:47:19 -0500

    Added many tests

--- test/node-config-webpack.spec.js
+++ test/node-config-webpack.spec.js
@@ -3,7 +3,7 @@ import { describe, it } from "mocha";
 import path from "path";
 import webpack from "webpack";
 
-import ConfigWebpackPlugin from "../src/ConfigWebpackPlugin";
+import ConfigWebpackPlugin from "../index";
 
 describe("Config Webpack Plugin", function() {
     this.timeout(5000);
@@ -13,43 +13,64 @@ describe("Config Webpack Plugin", function() {
 
         webpack({
             entry: "./test-resources/testDefaultNS",
-            output: { filename: "testDefaultNS.js" }, // dist/testOutput.js
+            output: {
+                path: path.join(__dirname, "../dist"),
+                filename: "testDefaultNS.js"
+            },
             plugins: [
                 plugin
             ]
         }, (err, stats) => {
+            if (err) {
+                assert.fail();
+            }
+
             const out = require("../dist/testDefaultNS");
 
             done();
         });
     });
 
-    it("Produces a plugin with a custom namespace", function() {
+    it("Produces a plugin with a custom namespace", function(done) {
         const plugin = new ConfigWebpackPlugin("myConfig");
 
         webpack({
             entry: "./test-resources/testCustomNS",
-            output: { filename: "testCustomNS.js" }, // dist/testOutput.js
+            output: {
+                path: path.join(__dirname, "../dist"),
+                filename: "testCustomNS.js"
+            },
             plugins: [
                 plugin
             ]
         }, (err, stats) => {
+            if (err) {
+                assert.fail();
+            }
+
             const out = require("../dist/testCustomNS");
 
             done();
         });
     });
 
-    it("Produces a plugin with a blank namespace", function() {
+    it("Produces a plugin with a blank namespace", function(done) {
         const plugin = new ConfigWebpackPlugin("");
 
         webpack({
             entry: "./test-resources/testBlankNS",
-            output: { filename: "testBlankNS.js" }, // dist/testOutput.js
+            output: {
+                path: path.join(__dirname, "../dist"),
+                filename: "testBlankNS.js"
+            },
             plugins: [
                 plugin
             ]
         }, (err, stats) => {
+            if (err) {
+                assert.fail();
+            }
+
             const out = require("../dist/testBlankNS");
 
             done();
